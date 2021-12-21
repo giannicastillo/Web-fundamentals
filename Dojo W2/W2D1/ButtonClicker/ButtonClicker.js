886005e18b3233c99dd28142183d02ee985ac92b
giannicastillo <gianni.castillo96@gmail.com>
2021-12-21 17:21:27 -0500

    ButtonClicker complete!

---
+++ Dojo W2/W2D1/ButtonClicker/ButtonClicker.js
@@ -0,0 +1,10 @@
+// let element= document.getElementById(this)
+function loginout(element){
+    element.innerText = "Log Out";
+}
+
+function hide(element){
+    element.remove();
+}
+//element.remove
+//element.style.display = none 
