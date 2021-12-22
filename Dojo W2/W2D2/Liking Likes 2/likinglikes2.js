70f284c795fceecc5c26dc4c894350adc8a847ff
giannicastillo <gianni.castillo96@gmail.com>
2021-12-22 01:20:38 -0500

    likinglikes assignment completed!

---
+++ Dojo W2/W2D2/Liking Likes 2/likinglikes2.js
@@ -0,0 +1,30 @@
+let likebtn1 = 9
+var likebtn1Element = document.querySelector("#numlikes-1");
+
+console.log(likebtn1Element)
+
+function add1(){
+    likebtn1++;
+    likebtn1Element.innerText = likebtn1 + "like(s)"
+    console.log(likebtn1)
+}
+let likebtn2 = 12
+var likebtn2Element = document.querySelector("#numlikes-2");
+
+console.log(likebtn2Element)
+
+function add2(){
+    likebtn2++;
+    likebtn2Element.innerText = likebtn2 + "like(s)"
+    console.log(likebtn2)
+}
+let likebtn3 = 9
+var likebtn3Element = document.querySelector("#numlikes-3")
+
+console.log(likebtn3)
+
+function add3(){
+    likebtn3++;
+    likebtn3Element.innerText = likebtn3 + "like(s)"
+    console.log(likebtn3)
+}
